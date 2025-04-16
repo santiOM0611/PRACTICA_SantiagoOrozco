ff49cf3e52aa672ae623658e0b412052fdc148eb
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-04-16 00:29:07 -0600

    Administrar usuarios (Completado)
    Práctica finalizada.

---
+++ PRACTICA_SantiagoOrozco/src/main/java/PRACTICA/PRACTICA/controller/UsuarioController.java
@@ -0,0 +1,49 @@
+package PRACTICA.PRACTICA.controller;
+
+import PRACTICA.PRACTICA.domain.Usuario;
+import PRACTICA.PRACTICA.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var usuarios = usuarioService.getUsuarios();
+        model.addAttribute("usuarios", usuarios);
+        model.addAttribute("totalUsuarios", usuarios.size());
+        return "/usuario/listado";
+    }
+    
+    @GetMapping("/nuevo")
+    public String usuarioNuevo(Usuario usuario) {
+        return "/usuario/modifica";
+    }
+    
+    @PostMapping("/guardar")
+    public String usuarioGuardar(Usuario usuario) {
+        usuarioService.save(usuario, true);
+        return "redirect:/usuario/listado";
+    }
+    
+    @GetMapping("/eliminar/{idUsuario}")
+    public String usuarioEliminar(Usuario usuario) {
+        usuarioService.delete(usuario);
+        return "redirect:/usuario/listado";
+    }
+    
+    @GetMapping("/modificar/{idUsuario}")
+    public String usuarioModificar(Usuario usuario, Model model) {
+        usuario = usuarioService.getUsuario(usuario);
+        model.addAttribute("usuario", usuario);
+        return "/usuario/modifica";
+    }
+}
---
+++ PRACTICA_SantiagoOrozco/src/main/java/PRACTICA/PRACTICA/dao/RolDao.java
@@ -0,0 +1,8 @@
+package PRACTICA.PRACTICA.dao;
+
+import PRACTICA.PRACTICA.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
